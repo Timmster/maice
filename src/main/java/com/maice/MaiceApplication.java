9856c55189b7096e5a5a3bb82b12a552451d2e17
Timmster <timm.schwemann@googlemail.com>
2017-02-13 12:28:17 +0100

    Update MaiceApplication.java

--- src/main/java/com/maice/MaiceApplication.java
+++ src/main/java/com/maice/MaiceApplication.java
@@ -1,13 +1,26 @@
 package com.maice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class MaiceApplication {
+public class MaiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaiceApplication.class, args);
 	}
+	
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(MaiceApplication.class);
+  }
 
 }
