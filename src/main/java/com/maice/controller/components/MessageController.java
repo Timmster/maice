884f4e8f62275a974e53f36b42325d5419574986
Timmster <timm.schwemann@googlemail.com>
2017-02-13 12:29:25 +0100

    Update MessageController.java

--- src/main/java/com/maice/controller/components/MessageController.java
+++ src/main/java/com/maice/controller/components/MessageController.java
@@ -1,7 +1,5 @@
 package com.maice.controller.components;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Properties;
@@ -12,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
-import com.maice.model.MaiceConstants;
 import com.maice.model.dto.MaiceServerResponse;
 
 @Component
@@ -23,24 +20,23 @@ public class MessageController extends AbstractMaiceController {
 
 	private static HashMap<String, Properties> messagesForCountryCode = new HashMap<String, Properties>();
 
+	private static final String[] languages = {"de", "en"};
+	
+	
 	public MessageController() {
 		if (MessageController.messagesForCountryCode.size() == 0) {
-			String[] files = new File(MaiceConstants.PATH_LANGUAGES).list();
-			for (String f : files) {
-				loadPropertiesFile(f.substring(9, 11), f);
+			for (String lang : languages) {
+			  try {
+			    InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("messages_" + lang + ".properties");
+//			    InputStream in = getClass().getResourceAsStream("messages_" + lang + ".properties");
+		      Properties p = new Properties();
+		      p.load(in);
+		      in.close();
+		      messagesForCountryCode.put(lang, p);
+		    } catch (Exception e) {
+		      LOG.error("Could not load Properties File " + lang + ". Exception: " + e.getMessage());
+		    }
 			}
-		}
-	}
-
-	private void loadPropertiesFile(String countryCode, String filename) {
-		try {
-			InputStream in = new FileInputStream(new File(MaiceConstants.PATH_LANGUAGES + "/" + filename));
-			Properties p = new Properties();
-			p.load(in);
-			in.close();
-			messagesForCountryCode.put(countryCode, p);
-		} catch (Exception e) {
-			LOG.error("Could not load Properties File " + filename + ". Exception: " + e.getMessage());
 		}
 	}
 
